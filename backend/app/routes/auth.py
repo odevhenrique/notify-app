04a7e5cac07588cda67b3d92f1f1fc4db597c2ca
Henrique Costa <costa.henrique9577@gmail.com>
2026-04-18 13:32:32 -0300

    feat: cria senha hash

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -3,6 +3,7 @@ from app.database.connection import SessionLocal
 from app.schemas.user import UserCreate, UserLogin
 from sqlalchemy.orm import Session
 from app.models.user import User
+from app.core.security import hash_password, verify_password
 
 router = APIRouter()
 
@@ -24,7 +25,7 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     new_user = User(
         name = user.name,
         email = user.email,
-        password = user.password # criptografar depois!!!!
+        password = hash_password(user.password)
     )
 
     db.add(new_user)
@@ -37,7 +38,7 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 def login(user: UserLogin, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user.email).first()
 
-    if not db_user or db_user.password != user.password:
+    if not db_user or not verify_password(user.password, db_user.password):
         raise HTTPException(status_code=400, detail="Credencias inválidas")
     
     return {"message": "Login realizado com sucesso"}

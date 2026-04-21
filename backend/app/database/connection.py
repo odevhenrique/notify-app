9d2552a58d28e3e743181487969494f997613806
Henrique Costa <costa.henrique9577@gmail.com>
2026-04-21 18:42:53 -0300

    excluir print de url

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -7,8 +7,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-print("DATABASE_URL:", DATABASE_URL)
-
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
